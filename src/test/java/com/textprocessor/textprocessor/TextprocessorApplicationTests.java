b734dfd51b80b60098d14904d5adac3052e2067c
origin <kruglov56832@mail.ru>
2025-03-28 17:39:40 +0900

    text_processor_1

---
+++ src/test/java/com/textprocessor/textprocessor/TextprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textprocessor.textprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
